73c166f00d4af2c5adfb82f11d666dee03829f32
AziraalDev <dev.evgeniy.alekseev@gmail.com>
2024-06-13 13:51:41 +0300

    hw_15_start

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 use App\Enums\HttpStatus as HttpStatus;
 use Core\Router as Router;
+use Dotenv\Dotenv;
 
 #parent directory of "public" -> html
 define('BASE_DIR', dirname(__DIR__));
@@ -9,6 +10,8 @@ define('BASE_DIR', dirname(__DIR__));
 require_once BASE_DIR . "/vendor/autoload.php";
 
 try {
+    $dotenv = Dotenv::createUnsafeImmutable(BASE_DIR);
+
     die(Router::dispatch($_SERVER['REQUEST_URI']));
 } catch (Throwable $exception) {
     die(jsonResponse(
